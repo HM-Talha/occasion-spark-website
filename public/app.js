72cbd1837e0cab89ad2814546e96607ec8d5eb03
talha <hmttalha4@gmail.com>
2021-12-28 01:16:14 +0500

    Occasion Sparks

---
+++ public/app.js
@@ -0,0 +1,14 @@
+function textHide(e) {
+    var headText = document.getElementById("headText");
+    console.log(e.id);
+    if (e.id === "1") {
+        headText.style.display = "none"
+        e.id = "2"
+    }
+    else {
+        headText.style.display = "block"
+        e.id = "1"
+
+    }
+}
+
